561a71dafaa73a083d854d0ff7c415c87a8601bf
vivek <vivek@gmail.com>
2023-08-14 14:22:26 +0530

    Javascript updated

---
+++ pagescript.js
@@ -0,0 +1,56 @@
+// JS is a dynamically typed language
+// It is an Object Oriented Scripting Language
+// It is a single threaded Scripting language
+
+console.log("Hello Javascript")
+x=100
+console.log(x)
+console.log("the value of x = ",x)
+// Data types : typeof() : return the data type
+// number : int,  float
+console.log(typeof(x))
+
+y=12.344
+console.log(typeof(y))
+
+// string
+x="Hello"
+console.log(typeof(x))
+
+// boolean
+x=true 
+console.log(typeof(x))
+
+// undefined
+x=undefined
+console.log(typeof(x))
+
+// null
+x=null
+console.log(typeof(x))
+
+
+// warning message
+console.warn("your password is weak")
+
+console.error("Password not matched")
+
+// alert box
+
+// alert("Hello JS")
+
+//Operators
+/*
+
+Arithmatic operator
+  +, - , * , / , %
+  
+   ** : power
+
+*/
+
+t=16**4
+console.log(t)
+
+// relational operator
+// >,<,>=, <=, !=, ==, ===
